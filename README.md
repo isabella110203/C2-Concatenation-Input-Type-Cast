f92b3ae3afffb69658251093d6350c812a42bf58
isabella110203 <110203@eesd.org>
2021-01-21 23:33:55 +0000

    More info added.

--- README.md
+++ README.md
@@ -13,3 +13,17 @@ Forcing a data type on a variable
 
 ### To force a string into an integer
 `int(variableName)`
+
+## Input
+
+Input is the way the computer gets information from the user
+
+Python initially treats input as __string__ type
+
+Ex. 
+5 --> "5"
+Today is Thursday --> "Today is Thursday"
+
+### Formula
+'variableName = input("Optional Prompt)
+Python stores the __input__ in the __variableName__
--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 print("Hello" + "World")
 print("Hello" + " " + "World" + "Today is Thursday.")
 # This is a concatenation of strings.
+print()
+print("Tomorrow is Friday.")
+
+# If adding strings is called "concatenation"
+# What is the + for integers called?
+# 5 + 4 = 9
+
+print("5" + "4")
+print(5 + 4)
+
+firstName = "Isabella" #string
+print("My name is " + firstName + ".")
+age = 12 #integer
+print("My age is " + str(age) + ".")
+
+# firstName != FirstName !(not)=(equal) 
+
+userName = input("What is your name? ")
+print("Hello " + userName)
+
+# Ask the user for their birth year
+userBirth = int(input("What year were you born? "))
+## print(2021-userBirth)
+print("You are " + str(2021-userBirth) + "years old.")
+
